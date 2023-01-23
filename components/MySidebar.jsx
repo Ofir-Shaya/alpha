d3262644f87ac6a929db7c66677bdb71c00e2a4d
Ofir-Shaya <ofirshaya8@gmail.com>
2023-01-23 23:48:56 +0200

    update sidebar

--- components/MySidebar.jsx
+++ components/MySidebar.jsx
@@ -1,71 +1,80 @@
-import React from 'react';
-import { Text, useTheme, Grid } from '@nextui-org/react';
+import React from "react";
+import { Link, useTheme, Grid, Spacer } from "@nextui-org/react";
 import {
-    MoonRounded,
-    SunRounded,
-    Money,
-    FishBone,
-    Flag,
-    FlipFlops,
-} from './Icons/AllIcons';
+  MoonRounded,
+  SunRounded,
+  Money,
+  FishBone,
+  Flag,
+  FlipFlops,
+} from "./Icons/AllIcons";
 
 export const MySidebar = () => {
-    const { isDark } = useTheme();
+  const { isDark } = useTheme();
 
-    return (
-        <Grid.Container
-            display='flex'
-            justify='flex-start'
-            direction='column'
-            alignItems='flex-start'
-            css={{
-                margin: '0',
-                width: '15%',
-                backgroundColor: isDark ? '#0B0B0B' : '#B0B0B0',
-                height: '100vh',
-                paddingTop: '25px',
-            }}>
-            <Grid xs={4} css={{ marginLeft: '50px' }}>
-                <FlipFlops width='26' height='26' />
-                <Text b color='inherit' hideIn='sm'>
-                    Sandals
-                </Text>
-            </Grid>
+  return (
+    <Grid.Container
+      display="flex"
+      justify="flex-start"
+      direction="column"
+      alignItems="flex-start"
+      css={{
+        margin: "0",
+        width: "fitContent",
+        backgroundColor: isDark ? "#0B0B0B" : "#B0B0B0",
+        height: "100vh",
+        paddingTop: "25px",
+      }}
+    >
+      <Grid xs={1} css={{ marginLeft: "50px" }}>
+        <Link b color="inherit" hideIn="sm">
+          <FlipFlops width="26" height="26" />
+          Sandals
+        </Link>
+      </Grid>
+      <Spacer y={1} />
 
-            <Grid xs={4} css={{ marginLeft: '50px' }}>
-                <MoonRounded width='26' height='26' />
-                <Text b color='inherit' hideIn='sm'>
-                    Moon
-                </Text>
-            </Grid>
+      <Grid xs={1} css={{ marginLeft: "50px" }}>
+        <Link b color="inherit" hideIn="sm">
+          <MoonRounded width="26" height="26" />
+          Moon
+        </Link>
+      </Grid>
+      <Spacer y={1} />
 
-            <Grid xs={4} css={{ marginLeft: '50px' }}>
-                <SunRounded width='26' height='26' />
-                <Text b color='inherit' hideIn='sm'>
-                    Rounded
-                </Text>
-            </Grid>
+      <Grid xs={1} css={{ marginLeft: "50px" }}>
+        <Link b color="inherit" hideIn="sm">
+          <SunRounded width="26" height="26" />
+          Rounded
+        </Link>
+      </Grid>
+      <Spacer y={1} />
 
-            <Grid xs={4} css={{ marginLeft: '50px' }}>
-                <Money width='26' height='26' />
-                <Text b color='inherit' hideIn='sm'>
-                    Money
-                </Text>
-            </Grid>
+      <Grid xs={1} css={{ marginLeft: "50px" }}>
+        <Link b color="inherit" hideIn="sm">
+          {" "}
+          <Money width="26" height="26" />
+          Money
+        </Link>
+      </Grid>
+      <Spacer y={1} />
 
-            <Grid xs={4} css={{ marginLeft: '50px' }}>
-                <FishBone width='26' height='26' />
-                <Text b color='inherit' hideIn='sm'>
-                    Fish
-                </Text>
-            </Grid>
+      <Grid xs={1} css={{ marginLeft: "50px" }}>
+        <Link b color="inherit" hideIn="sm">
+          {" "}
+          <FishBone width="26" height="26" />
+          Fish
+        </Link>
+      </Grid>
+      <Spacer y={1} />
 
-            <Grid xs={4} css={{ marginLeft: '50px' }}>
-                <Flag width='26' height='26' />
-                <Text b color='inherit' hideIn='sm'>
-                    Capture
-                </Text>
-            </Grid>
-        </Grid.Container>
-    );
+      <Grid xs={1} css={{ marginLeft: "50px" }}>
+        <Link b color="inherit" hideIn="sm">
+          {" "}
+          <Flag width="26" height="26" />
+          Capture
+        </Link>
+      </Grid>
+    </Grid.Container>
+  );
 };
