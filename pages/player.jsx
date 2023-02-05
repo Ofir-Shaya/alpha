068508885ebc2e5f5c819a7970832471ea951fe7
Ofir-Shaya <ofirshaya8@gmail.com>
2023-02-05 19:30:37 +0200

    update player page

--- pages/player.jsx
+++ pages/player.jsx
@@ -1,136 +1,206 @@
-import MyNavbar from '@/components/MyNavbar';
-import MySidebar from '@/components/MySidebar';
-import { Container, Grid, Text, Card, Loading } from '@nextui-org/react';
-import { useEffect, useState } from 'react';
+import MyNavbar from "@/components/MyNavbar";
+import MySidebar from "@/components/MySidebar";
+import { Container, Grid, Text, Card, Loading } from "@nextui-org/react";
+import { useEffect, useState } from "react";
+import Champs from "@/public/json/champions.json";
 
-const jsondata = {};
+const champsData = Champs;
 
-const findIdByKey = (key, json) => {
-    const data = Object.values(json.data);
-    let left = 0;
-    let right = data.length - 1;
+const championMap = new Map();
+for (const [key, value] of Object.entries(champsData)) {
+  championMap.set(value.key, value.name);
+}
+const getChampionName = (key) => championMap.get(key);
 
-    while (left <= right) {
-        const middle = Math.floor((left + right) / 2);
-        if (data[middle].key === key) {
-            return data[middle].id;
-        }
-        if (data[middle].key < key) {
-            left = middle + 1;
-        } else {
-            right = middle - 1;
-        }
+const useLocalStorageData = (localStorageKey) => {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    try {
+      setData(JSON.parse(localStorage.getItem(localStorageKey)));
+    } catch (error) {
+      console.error(`Failed to retrieve data from local storage: ${error}`);
     }
-    return null;
+  }, []);
+  return data;
 };
-
-const json = {
-    // your json file here
-    // ...
-};
-
-const dataHashMap = new Map();
-Object.values(json.data).forEach((item) => {
-    dataHashMap.set(item.key, item);
-});
-
-const hashfindIdByKey = (key, dataHashMap) => {
-    return dataHashMap.get(key) ? dataHashMap.get(key).id : null;
-};
-
-const id = hashfindIdByKey(266, dataHashMap); // will return 'Aatrox'
-console.log(id);
 
 const PlayerCard = ({ player, objectProp }) => {
-    return (
-        <Card css={{ h: '$24', $$cardColor: '$colors$secondary' }}>
-            <Card.Body>
-                <Text h6 size={28} color='white' css={{ mt: 0 }}>
-                    {player[objectProp]}
-                </Text>
-            </Card.Body>
-        </Card>
-    );
+  const { [objectProp]: value } = player;
+  return (
+    <Card css={{ h: "$24", $$cardColor: "$colors$secondary" }}>
+      <Card.Body>
+        <Text
+          h6
+          size={28}
+          css={{
+            textGradient: "45deg, $yellow600 -20%, $red600 100%",
+            mt: 0,
+            textAlign: "center",
+          }}
+        >
+          {value}
+        </Text>
+      </Card.Body>
+    </Card>
+  );
 };
 
 const MasteryCard = ({ mastery, objectProp }) => {
-    console.log(mastery);
-    return (
-        <Card css={{ h: '$24', $$cardColor: '$colors$secondary' }}>
-            <Card.Body>
-                <Text h6 size={28} color='white' css={{ mt: 0 }}>
-                    {mastery[objectProp]}
-                </Text>
-            </Card.Body>
-        </Card>
-    );
+  const { [objectProp]: key } = mastery;
+  const championName =
+    objectProp === "championId" ? getChampionName(String(key)) : null;
+
+  return (
+    <Card css={{ h: "$24", $$cardColor: "$colors$secondary" }}>
+      <Card.Body>
+        <Text
+          h6
+          size={28}
+          css={{
+            textGradient: "45deg, $yellow600 -20%, $red600 100%",
+            mt: 0,
+            textAlign: "center",
+          }}
+          weight="bold"
+        >
+          {championName || key}
+        </Text>
+      </Card.Body>
+    </Card>
+  );
 };
 
 const Player = () => {
-    const [player, setPlayer] = useState(null);
-    const [mastery, setMastery] = useState(null);
+  const player = useLocalStorageData("player");
+  const mastery = useLocalStorageData("mastery");
 
-    useEffect(() => {
-        setPlayer(JSON.parse(localStorage.player));
-        setMastery(JSON.parse(localStorage.mastery));
-    }, []);
+  return (
+    <>
+      <MyNavbar />
+      <Container
+        display="flex"
+        direction="row"
+        wrap="noWrap"
+        css={{ margin: 0, padding: 0 }}
+      >
+        <MySidebar />
 
-    return (
-        <>
-            <MyNavbar />
-            <Container
-                display='flex'
-                direction='row'
-                wrap='noWrap'
-                css={{ margin: 0, padding: 0 }}>
-                <MySidebar />
+        <Container fluid>
+          <Grid.Container gap={2} justify="center">
+            <Grid xs={6}>
+              {player ? (
+                <>
+                  <Text
+                    size={28}
+                    css={{
+                      display: "flex",
+                      alignItems: "center",
+                      textAlign: "center",
+                    }}
+                  >
+                    Name:{" "}
+                  </Text>
+                  <PlayerCard player={player} objectProp="name" />
+                </>
+              ) : (
+                <Loading />
+              )}
+            </Grid>
+            <Grid xs={6}>
+              {player ? (
+                <>
+                  <Text
+                    size={28}
+                    css={{
+                      display: "flex",
+                      alignItems: "center",
+                      textAlign: "center",
+                    }}
+                  >
+                    Summoner Level:{" "}
+                  </Text>
+                  <PlayerCard player={player} objectProp="summonerLevel" />
+                </>
+              ) : (
+                <Loading />
+              )}
+            </Grid>
 
-                <Container fluid>
-                    <Grid.Container gap={2} justify='center'>
-                        <Grid xs={4}>
-                            {player ? (
-                                <>
-                                    <Text>Name- </Text>
-                                    <PlayerCard
-                                        player={player}
-                                        objectProp='name'
-                                    />
-                                </>
-                            ) : (
-                                <Loading />
-                            )}
-                        </Grid>
-                        <Grid xs={4}>
-                            {player ? (
-                                <>
-                                    <Text>Summoner Level- </Text>
-                                    <PlayerCard
-                                        player={player}
-                                        objectProp='summonerLevel'
-                                    />
-                                </>
-                            ) : (
-                                <Loading />
-                            )}
-                        </Grid>
-                        <Grid xs={4}>
-                            {mastery ? (
-                                <>
-                                    <Text>Champion- </Text>
-                                    <MasteryCard
-                                        mastery={mastery[0]}
-                                        objectProp='championId'
-                                    />
-                                </>
-                            ) : (
-                                <Loading />
-                            )}
-                        </Grid>
-                    </Grid.Container>
-                </Container>
-            </Container>
-        </>
-    );
+            <Grid xs={12}>
+              <Grid.Container>
+                <Grid xs={4}>
+                  {mastery ? (
+                    <>
+                      <Text
+                        size={28}
+                        css={{
+                          display: "flex",
+                          alignItems: "center",
+                          textAlign: "center",
+                        }}
+                      >
+                        Most played Champion:{" "}
+                      </Text>
+                      <MasteryCard
+                        mastery={mastery[0]}
+                        objectProp="championId"
+                      />
+                    </>
+                  ) : (
+                    <Loading />
+                  )}
+                </Grid>
+                <Grid xs={4}>
+                  {mastery ? (
+                    <>
+                      <Text
+                        size={28}
+                        css={{
+                          display: "flex",
+                          alignItems: "center",
+                          textAlign: "center",
+                        }}
+                      >
+                        Mastery points:{" "}
+                      </Text>
+                      <MasteryCard
+                        mastery={mastery[0]}
+                        objectProp="championPoints"
+                      />
+                    </>
+                  ) : (
+                    <Loading />
+                  )}
+                </Grid>
+                <Grid xs={4}>
+                  {mastery ? (
+                    <>
+                      <Text
+                        size={28}
+                        css={{
+                          display: "flex",
+                          alignItems: "center",
+                          textAlign: "center",
+                        }}
+                      >
+                        Mastery level:{" "}
+                      </Text>
+                      <MasteryCard
+                        mastery={mastery[0]}
+                        objectProp="championLevel"
+                      />
+                    </>
+                  ) : (
+                    <Loading />
+                  )}
+                </Grid>
+              </Grid.Container>
+            </Grid>
+          </Grid.Container>
+        </Container>
+      </Container>
+    </>
+  );
 };
 
 export default Player;
